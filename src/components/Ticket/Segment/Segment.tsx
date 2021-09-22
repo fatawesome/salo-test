f580bbd893fe04782571fc00d2b14d25c399d016
Ilya Alonov <fatawesomeee@gmail.com>
2021-09-22 20:23:03 +0300

    add stops pluralization

--- src/components/Ticket/Segment/Segment.tsx
+++ src/components/Ticket/Segment/Segment.tsx
@@ -9,6 +9,33 @@ interface SegmentProps {
   className?: string;
 }
 
+/**
+ Нет пересадок
+ 1 пересадка   1     group A
+ 2 пересадки   2-4   group B
+ 5 пересадок   5-10  group C
+ */
+const getPluralizedStopsText = (stops: string[]) => {
+  // imperative stuff
+  if (stops.length === 0) {
+    return 'Нет пересадок';
+  }
+
+  const numOfStops = stops.length;
+  const baseText = `${numOfStops} пересад`;
+
+  const reminder = numOfStops % 10;
+  const isPluralGroupA = reminder === 1;
+  const isPluralGroupB = (numOfStops < 5 || numOfStops > 21) && (reminder >= 2 && reminder <= 4);
+
+  if (isPluralGroupA) {
+    return baseText + 'ка';
+  } else if (isPluralGroupB) {
+    return baseText + 'ки';
+  }
+  return baseText + 'ок';
+}
+
 const Segment: React.FC<SegmentProps> = ({ segment, className }) => {
   return (
     <div className={className}>
@@ -21,7 +48,7 @@ const Segment: React.FC<SegmentProps> = ({ segment, className }) => {
         information={getRouteTime(segment)}
       />
       <LabeledInfo
-        label={`${segment.stops.length} пересадки`} // TODO: pluralization.
+        label={getPluralizedStopsText(segment.stops)}
         information={segment.stops.join(', ')}
       />
     </div>
@@ -30,8 +57,8 @@ const Segment: React.FC<SegmentProps> = ({ segment, className }) => {
 
 // TODO: make correct layout (try grids u stupid).
 const StyledSegment = styled(Segment)`
-  display: flex;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
 `
 
 export default StyledSegment;
