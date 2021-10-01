e045f552a2ceb45cd394766db352e3e484f142df
Ilya Alonov <fatawesomeee@gmail.com>
2021-10-01 14:25:15 +0500

    add: test renderer wrappers with styled-component theme

--- src/components/common/Button/spec/Button.spec.tsx
+++ src/components/common/Button/spec/Button.spec.tsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { ThemeProvider } from 'styled-components';
-import { aviasalesTheme } from '../../../../theme/theme';
 
 import Button from '../Button';
+import { snapshotWithTheme, uiWithTheme } from '../../../../theme/utils';
 
 describe('Button component', () => {
   const buttonText = 'Click me'
   const clickHandler = jest.fn();
 
   test('renders correctly', () => {
-    // TODO: сделать враппер над рендерером, чтобы не прокидывать тему каждый раз
-    const component = renderer.create(
-      <ThemeProvider theme={aviasalesTheme}>
-        <Button onClick={clickHandler}>{buttonText}</Button>
-      </ThemeProvider>
+    const component = snapshotWithTheme(
+      <Button onClick={clickHandler}>{buttonText}</Button>
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   })
 
   test('is clickable', () => {
-    render(
-      <ThemeProvider theme={aviasalesTheme}>
-        <Button onClick={clickHandler}>{buttonText}</Button>
-      </ThemeProvider>
-    );
+    uiWithTheme(<Button onClick={clickHandler}>{buttonText}</Button>)
     userEvent.click(screen.getByText(buttonText));
     expect(clickHandler).toBeCalledTimes(1);
   });
