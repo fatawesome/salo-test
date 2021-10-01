d7854d720ab26bff1e273f32e0d43b32d720d9bb
Ilya Alonov <fatawesomeee@gmail.com>
2021-10-01 19:04:26 +0500

    fix: lol you need to init module before testing

--- src/models/tickets/__tests__/tickets.spec.ts
+++ src/models/tickets/__tests__/tickets.spec.ts
@@ -2,13 +2,19 @@ import { allSettled, fork } from 'effector';
 import { $tickets, fetchTicketsFx, initSearchFx, searchInitiated } from '../index';
 import { testTickets } from '../../../stubs';
 
+// лол я два часа думал, почему тест не работает только чтобы понять,
+// что я не запустил инициализацию))0)0
+import '../init';
+
 describe('Tickets fetching module', () => {
   test('sets tickets after initiating search', async () => {
-    let initSearchHandler = () => 'FAKE_SEARCH_ID';
-    let fetchTicketsHandler = () => ({
-      tickets: testTickets,
-      stop: false
-    });
+    let initSearchHandler = jest.fn(() => 'FAKE_SEARCH_ID');
+    let fetchTicketsHandler = jest.fn(
+      () => ({
+        tickets: testTickets,
+        stop: false
+      })
+    );
 
     const scope = fork({
       handlers: new Map()
